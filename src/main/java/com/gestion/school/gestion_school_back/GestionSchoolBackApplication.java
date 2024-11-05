6df6a3d2130c63066290bdad143e6af82d1c9a57
ChristianRey117 <51842236+ChristianRey117@users.noreply.github.com>
2024-11-04 21:55:09 -0500

    init commit project

---
+++ src/main/java/com/gestion/school/gestion_school_back/GestionSchoolBackApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.school.gestion_school_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionSchoolBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionSchoolBackApplication.class, args);
+	}
+
+}
